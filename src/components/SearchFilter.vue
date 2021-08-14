2865e0889ef46c3feafec25e1a7de4eeff65fd44
matiussi <matiussi305@gmail.com>
2021-08-14 02:09:10 -0300

    Mobile header layout

--- src/components/SearchFilter.vue
+++ src/components/SearchFilter.vue
@@ -1,16 +1,47 @@
 <template>
-  <div class="search-filter">
-    <input placeholder="Add location" class="input-location"/>
-    <input placeholder="Add guests" class="input-guests" />
-    <button class="search-button"> <span class="material-icons">search</span></button>
+  <div class="search-details" :class="{selected}">
+    <div class="search-top" v-if="selected">
+      <h3>Edit your search</h3>
+      <span class="material-icons close-button"  @click="selected = false">close</span>
+    </div>
+    <div class="search-filter" >
+      <div class="input-location">
+        <label v-if="selected">LOCATION</label>
+        <input placeholder="Add location" @click="selected = !seleted"/>
+      </div>
+      <div class="input-guests">
+        <label v-if="selected">GUESTS</label>
+        <input placeholder="Add guests" @click="selected = !seleted"/>
+      </div>
+      <button class="search-button" v-if="!selected"> <span class="material-icons search">search</span></button>
+    </div>
+    <ul class="search-results" v-if="selected">
+      <li class="search-item"><span class="material-icons">location_on</span>Helsinki, Finland</li>
+      <li class="search-item"><span class="material-icons">location_on</span>Helsinki, Finland</li>
+      <li class="search-item"><span class="material-icons">location_on</span>Helsinki, Finland</li>
+      <li class="search-item"><span class="material-icons">location_on</span>Helsinki, Finland</li>
+      <li class="search-item"><span class="material-icons">location_on</span>Helsinki, Finland</li>
+      <li class="search-item"><span class="material-icons">location_on</span>Helsinki, Finland</li>
+      <li class="search-item"><span class="material-icons">location_on</span>Helsinki, Finland</li>
+      <li class="search-item"><span class="material-icons">location_on</span>Helsinki, Finland</li>
+      <li class="search-item"><span class="material-icons">location_on</span>Helsinki, Finland</li>
+      <li class="search-item"><span class="material-icons">location_on</span>Helsinki, Finland</li>
+      <li class="search-item"><span class="material-icons">location_on</span>Helsinki, Finland</li>
+    </ul>
+    <span class="search-button" v-if="selected"> <span class="material-icons search">search</span>Search</span>
 
   </div>
 </template>
 
 <script>
-export default{
-  name: 'SearchFilter'
-}
+  export default{
+    name: 'SearchFilter',
+    data(){
+      return{
+        selected: false
+      }
+    }
+  }
 </script>
 
 
